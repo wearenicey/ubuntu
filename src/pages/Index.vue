1c3a4f2e931ea031da4b691d38f94b320ca586ef
Gongash92 <zarko.stevanovic092@gmail.com>
2021-09-22 12:48:57 +0200

    proba 4

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -209,6 +209,120 @@
 
 			<!-- Testimonial -->
 
+			<section class="bg-indigo-800 container mx-auto my-12 rounded-2xl">
+				<div class="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8 rounded-r-3xl">
+					<div class="py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-indigo-900 lg:pr-16">
+						<div class="md:flex-shrink-0">
+							<img class="h-12" src="https://tailwindui.com/img/logos/tuple-logo-indigo-300.svg" alt="Tuple" />
+						</div>
+						<blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
+							<div class="relative text-lg font-medium text-white md:flex-grow">
+								<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-indigo-600" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+									<path
+										d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"
+									/>
+								</svg>
+								<p class="relative">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.</p>
+							</div>
+							<footer class="mt-8">
+								<div class="flex items-start">
+									<div class="flex-shrink-0 inline-flex rounded-full border-2 border-white">
+										<img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+									</div>
+									<div class="ml-4">
+										<div class="text-base font-medium text-white">Judith Black</div>
+										<div class="text-base font-medium text-indigo-200">CEO, Tuple</div>
+									</div>
+								</div>
+							</footer>
+						</blockquote>
+					</div>
+					<div class="py-12 px-4 border-t-2 border-indigo-900 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
+						<div class="md:flex-shrink-0">
+							<img class="h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-300.svg" alt="Workcation" />
+						</div>
+						<blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
+							<div class="relative text-lg font-medium text-white md:flex-grow">
+								<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-indigo-600" fill="currentColor" viewBox="0 0 32 32">
+									<path
+										d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"
+									/>
+								</svg>
+								<p class="relative">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis. Nemo expedita voluptas culpa sapiente alias molestiae.
+								</p>
+							</div>
+							<footer class="mt-8">
+								<div class="flex items-start">
+									<div class="flex-shrink-0 inline-flex rounded-full border-2 border-white">
+										<img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+									</div>
+									<div class="ml-4">
+										<div class="text-base font-medium text-white">Joseph Rodriguez</div>
+										<div class="text-base font-medium text-indigo-200">CEO, Workcation</div>
+									</div>
+								</div>
+							</footer>
+						</blockquote>
+					</div>
+				</div>
+			</section>
+
+			<!-- End testimonial -->
+
+			<!-- Logo cloud section -->
+			<div class="mt-32">
+				<div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+					<div class="lg:grid lg:grid-cols-2 lg:gap-24 lg:items-center">
+						<div>
+							<h2 class="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">Backed by world-renowned investors</h2>
+							<p class="mt-6 max-w-3xl text-lg leading-7 text-gray-500">
+								Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi. Cursus faucibus nunc nisl netus morbi vel porttitor vitae ut.
+								Amet vitae fames senectus vitae.
+							</p>
+							<div class="mt-6">
+								<a href="#" class="text-base font-medium text-red-500"> Meet our investors and </a>
+							</div>
+						</div>
+						<div class="mt-12 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
+							<div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+								<img class="max-h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Transistor" />
+							</div>
+
+							<div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+								<img class="max-h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage" />
+							</div>
+
+							<div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+								<img class="max-h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple" />
+							</div>
+
+							<div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+								<img class="max-h-12" src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg" alt="Laravel" />
+							</div>
+
+							<div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+								<img class="max-h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit" />
+							</div>
+
+							<div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+								<img class="max-h-12" src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg" alt="Workcation" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+
+			<!-- end novo -->
+
+			<!-- blog -->
+
+			<!-- This example requires Tailwind CSS v2.0+ -->
+	
+
+			<!-- end blog -->
+
 		
 	</Layout>
 </template>
