10f227fc46e4aa8c9a1cc977f998d5cca3ac668d
Gongash92 <zarko.stevanovic092@gmail.com>
2021-09-27 12:31:59 +0200

    upload forma

--- src/pages/Karijera.vue
+++ src/pages/Karijera.vue
@@ -280,19 +280,162 @@
 			</div>
 		</div>
 
-		<div>
-			<form id="sign-up" class="form" name="sign-up" method="POST" netlify-honeypot="bot-field" data-netlify="true" novalidate enctype="multipart/form-data" @submit.prevent="submit">
-				<label for="files">
-					Add file
-					<input id="files" type="file" name="files"  />
-				</label>
-				<input type="submit" value="Submit" class="submit" :disabled="submitStatus === 'PENDING'" :aria-disabled="submitStatus === 'PENDING'" />
-			</form>
+		<div class="container mx-auto">
+			<div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
+				<h3 class="text-lg font-medium text-gray-900">Send us a message</h3>
+				<form class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8" form id="sign-up" name="sign-up" method="POST" netlify-honeypot="bot-field" data-netlify="true" novalidate enctype="multipart/form-data" @submit.prevent="submit">
+					>
+					<div>
+						<input type="hidden" name="form-name" value="contact" />
+						<p hidden>
+							<label> Don’t fill this out: <input name="bot-field" /> </label>
+						</p>
+						<label for="name" class="block text-sm font-medium text-gray-900">First name</label>
+						<div class="mt-1">
+							<input
+								:class="{ 'form-group--error border border-red-500': $v.formData.name.$error }"
+								class="form-group py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+								type="text"
+								name="name"
+								id="name"
+								:invalid="$v.formData.name.$error"
+								v-model="$v.formData.name.$model"
+							/>
+							<div class="p-5">
+								<p class="error text-red-500 text-xs italic" v-if="$v.formData.name.$error">Name is required</p>
+							</div>
+						</div>
+					</div>
+					<div>
+						<label for="last" class="block text-sm font-medium text-gray-900">Last name</label>
+					</div>
+					<div>
+						<label for="email" class="block text-sm font-medium text-gray-900">Email</label>
+						<div class="mt-1">
+							<input
+								:class="{ 'form-group--error border border-red-500': $v.formData.email.$error }"
+								id="email"
+								name="email"
+								type="email"
+								class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+								:invalid="$v.formData.email.$error"
+								v-model="$v.formData.email.$model"
+							/>
+							<div class="p-5">
+								<p class="error text-red-500 text-xs italic" v-if="$v.formData.email.$error">Unesite isrpavnu email adresu</p>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div class="flex justify-between">
+							<label for="phone" class="block text-sm font-medium text-gray-900">Phone</label>
+							<span id="phone-optional" class="text-sm text-gray-500">Optional</span>
+						</div>
+						<div class="mt-1">
+							<input
+								v-model="formData.phone"
+								type="text"
+								name="phone"
+								id="phone"
+								autocomplete="tel"
+								class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+								aria-describedby="phone-optional"
+							/>
+						</div>
+					</div>
+					<input type="hidden" id="city" name="city" v-model="formData.city" />
+
+					<div class="sm:col-span-2">
+						<label for="subject" class="block text-sm font-medium text-gray-900">Subject</label>
+						<div class="mt-1">
+							<input
+								:class="{ 'form-group--error border border-red-500': $v.formData.subject.$error }"
+								type="text"
+								name="subject"
+								id="subject"
+								class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+								:invalid="$v.formData.subject.$error"
+								v-model="$v.formData.subject.$model"
+							/>
+							<div class="p-5">
+								<p class="error text-red-500 text-xs italic" v-if="$v.formData.subject.$error">Unesite naslov</p>
+							</div>
+						</div>
+					</div>
+					<div class="sm:col-span-2">
+						<div class="flex justify-between">
+							<label for="message" class="block text-sm font-medium text-gray-900">CV</label>
+						</div>
+						<div class="mt-1">
+							<div class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+								<div class="space-y-1 text-center">
+									<svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+										<path
+											d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+											stroke-width="2"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+										/>
+									</svg>
+									<div class="flex text-sm text-gray-600">
+										<label
+											for="file-upload"
+											class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+										>
+											<span>Upload a file</span>
+											<input id="files" type="file" name="files" @change="processFile($event)" class="sr-only" />
+										</label>
+										<p class="pl-1">or drag and drop</p>
+									</div>
+									<p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="sm:col-span-2 flex flex-wrap content-center sm:flex sm:justify-end p-5">
+						<div class="p-5">
+							<p class="typo__p" v-if="formData.submitStatus === 'OK'">Thanks for your submission!</p>
+							<p class="typo__p" v-if="formData.submitStatus === 'ERROR'">Please fill the form correctly.</p>
+							<p class="typo__p" v-if="formData.submitStatus === 'PENDING'">Sending...</p>
+						</div>
+						<div>
+							<button
+								:disabled="formData.submitStatus === 'PENDING'"
+								type="submit"
+								class="
+									mt-2
+									w-full
+									inline-flex
+									items-center
+									justify-center
+									px-6
+									py-3
+									border border-transparent
+									rounded-md
+									shadow-sm
+									text-base
+									font-medium
+									text-white
+									bg-indigo-600
+									hover:bg-indigo-700
+									focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+									sm:w-auto
+								"
+							>
+								Submit
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
 		</div>
+
 	</Layout>
 </template>
 <script>
 import CardItem from "../components/CardItem.vue";
+import { required, email } from "vuelidate/lib/validators";
+
 export default {
 	metaInfo: {
 		title: "Karijera | Ubuntu ",
@@ -300,11 +443,43 @@ export default {
 
 	data() {
 		return {
-			formData: {},
+			formData: {
+					name: "",
+				last: "",
+				email: "",
+				submitStatus: null,
+				phone: "",
+				files: {},
+				subject: "",
+			},
 		};
 	},
+
+	validations: {
+		formData: {
+			name: {
+				required,
+			},
+			last: {
+				required,
+			},
+			email: {
+				required,
+				email,
+			},
+			files: {
+				required,
+			},
+			subject: {
+				required,
+			},
+		},
+	},
+
 	methods: {
-	
+		processFile() {
+			this.formData.files = event.target.files;
+		},
 
 		encode(data) {
 			const formData = new FormData();
@@ -326,10 +501,14 @@ export default {
 					...this.formData,
 				}),
 			})
-				.then((res) => {
-					res.json();
-				})
-				.catch((err) => alert(err));
+				.then(
+					() => (this.formData.submitStatus = "PENDING"),
+
+					setTimeout(() => {
+						this.formData.submitStatus = "OK";
+					}, 500)
+				)
+				.catch((error) => alert(error));
 		},
 	},
 	components: {
