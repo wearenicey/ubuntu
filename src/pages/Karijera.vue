92338f732d5136b64fc0e2f75417bd3c0e70804f
Gongash92 <zarko.stevanovic092@gmail.com>
2021-09-27 13:33:04 +0200

    karijeraj upload

--- src/pages/Karijera.vue
+++ src/pages/Karijera.vue
@@ -623,13 +623,7 @@ export default {
 						...this.formData,
 					}),
 				})
-					.then(
-						() => (this.formData.submitStatus = "PENDING"),
-
-						setTimeout(() => {
-							this.formData.submitStatus = "OK";
-						}, 500)
-					)
+					.then(() => (this.formData.submitStatus = "OK"))
 					.catch((error) => alert(error));
 			}
 		},
