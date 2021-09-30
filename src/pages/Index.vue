0ad4c26299001545232fa2e9da7f81b0deb73657
Gongash92 <zarko.stevanovic092@gmail.com>
2021-09-30 15:41:03 +0200

    test 3

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -487,20 +487,16 @@ export default {
 	},
 	components: {
 		CardItem,
-		   BackToTop: () =>
-        import ('vue-backtotop')
-        .then(m => m.BackToTop)
-        .catch(),
 	},
 };
 </script>
 <style scoped>
 .btn-to-top {
-	width: 60px;
-	height: 60px;
-	padding: 10px 16px;
-	border-radius: 50%;
-	font-size: 22px;
-	line-height: 22px;
+  width: 60px;
+  height: 60px;
+  padding: 10px 16px;
+  border-radius: 50%;
+  font-size: 22px;
+  line-height: 22px;
 }
 </style>
