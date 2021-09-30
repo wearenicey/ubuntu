f80acdcd28da8e3752dba35955a2347b91faa7eb
Gongash92 <zarko.stevanovic092@gmail.com>
2021-09-30 15:33:51 +0200

    test 2

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -490,3 +490,13 @@ export default {
 	},
 };
 </script>
+<style scoped>
+.btn-to-top {
+	width: 60px;
+	height: 60px;
+	padding: 10px 16px;
+	border-radius: 50%;
+	font-size: 22px;
+	line-height: 22px;
+}
+</style>
