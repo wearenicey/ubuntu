6a4d863ef286cdb12233f5ad4e7df36e309bbc78
Gongash92 <your_email_address@example.com>
2021-10-01 14:30:13 +0200

    z index ispravljen

--- src/pages/ZastoUbuntu.vue
+++ src/pages/ZastoUbuntu.vue
@@ -6,14 +6,14 @@
 					<h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
 					<p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">What makes us different</p>
 				</div>
-				<div class="relative z-10 text-base max-w-prose mx-auto lg:max-w-5xl lg:mx-0 lg:pr-72">
+				<div class="relative z-1 text-base max-w-prose mx-auto lg:max-w-5xl lg:mx-0 lg:pr-72">
 					<p class="text-lg text-gray-500">
 						Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi. Cursus faucibus nunc nisl netus morbi vel porttitor vitae ut. Amet
 						vitae fames senectus vitae.
 					</p>
 				</div>
 				<div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-start">
-					<div class="relative z-10">
+					<div class="relative z-1">
 						<div class="prose prose-indigo text-gray-500 mx-auto lg:max-w-none">
 							<p>Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.</p>
 							<ul role="list">
