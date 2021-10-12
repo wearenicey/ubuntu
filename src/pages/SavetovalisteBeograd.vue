c83104bda6270e231c49056d1bb5c89dee849227
Gongash92 <zarko.stevanovic092@gmail.com>
2021-10-12 13:21:38 +0200

    ispravke

--- src/pages/SavetovalisteBeograd.vue
+++ src/pages/SavetovalisteBeograd.vue
@@ -14,8 +14,8 @@
 			<div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
 				<div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
 					<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-						<span class="block xl:inline">Beograd je vaš grad,</span>
-						<span class="block text-indigo-600 xl:inline">a treba vam psihoterapeut? Ubuntu je pravi izbor</span>
+						<span class="block xl:inline">Beograd je vaš grad, </span>
+						<span class="block xl:inline"> a treba vam <span class="text-indigo-600">psihoterapeut?</span> a treba vam Ubuntu je pravi izbor</span>
 					</h1>
 					<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">Beogradsko savetovalište Ubuntu se nalazi na Zelenom vencu. I na Novom Beogradu.</p>
 					<div class="mt-10 sm:flex sm:justify-center lg:justify-start">
@@ -217,14 +217,14 @@
 								</button>
 							</dt>
 							<dd x-show="open" class="mt-2 pr-12" id="faq-0">
-								<p class="text-base text-gray-500">Cena porodične psihoterapije</p>
+								<p class="text-base text-gray-500">Za 90 minuta psihoterapije je neophodno izdvojiti po osobi minimum 1.000,00 dinara, kao donacije za rad savetovališta.</p>
 							</dd>
 						</div>
 						<div x-data="{open: false}" class="pt-6">
 							<dt class="text-lg">
 								<!-- Expand/collapse question button -->
 								<button x-on:click="open = !open" type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
-									<span class="font-medium text-gray-900">Cena porodične psihoterapije</span>
+									<span class="font-medium text-gray-900">Koji su uslovi za ulazak u naš program i započinjanje psihoterapijskog procesa?</span>
 									<span class="ml-6 h-7 flex items-center">
 										<!--
                   Expand/collapse icon, toggle classes based on question open state.
