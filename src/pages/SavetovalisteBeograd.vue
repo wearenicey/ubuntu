dfab4eb9c42146c30de2495d074a7907c0d318de
Gongash92 <zarko.stevanovic092@gmail.com>
2021-10-12 12:57:47 +0200

    savetovaliste beograd

--- src/pages/SavetovalisteBeograd.vue
+++ src/pages/SavetovalisteBeograd.vue
@@ -1,20 +1,23 @@
 <template>
 	<div>
-			<back-to-top bottom="50px" right="50px" style="position: fixed; z-index: 99">
-			<button type="button" class="btn btn-info btn-to-top border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600"><svg class="icon" viewBox="0 0 16 16"> <title>Go to top of page</title> <g stroke-width="1" stroke="currentColor"> <polyline fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="15.5,11.5 8,4 0.5,11.5 "></polyline> </g> </svg></button>
+		<back-to-top bottom="50px" right="50px" style="position: fixed; z-index: 99">
+			<button type="button" class="btn btn-info btn-to-top border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600">
+				<svg class="icon" viewBox="0 0 16 16">
+					<title>Go to top of page</title>
+					<g stroke-width="1" stroke="currentColor"><polyline fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="15.5,11.5 8,4 0.5,11.5 "></polyline></g>
+				</svg>
+			</button>
 		</back-to-top>
 		<navigation-two></navigation-two>
-		
+
 		<main class="lg:relative">
 			<div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
 				<div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
 					<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-						<span class="block xl:inline">Data to enrich your</span>
-						<span class="block text-indigo-600 xl:inline">online business</span>
+						<span class="block xl:inline">Beograd je vaš grad,</span>
+						<span class="block text-indigo-600 xl:inline">a treba vam psihoterapeut? Ubuntu je pravi izbor</span>
 					</h1>
-					<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-						Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-					</p>
+					<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">Beogradsko savetovalište Ubuntu se nalazi na Zelenom vencu. I na Novom Beogradu.</p>
 					<div class="mt-10 sm:flex sm:justify-center lg:justify-start">
 						<div class="rounded-md shadow">
 							<a href="tel:+38158286883" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
@@ -26,7 +29,7 @@
 										d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
 									/>
 								</svg>
-								Zakažite besplatnu konsultaciju
+								Nazovi za besplatnu konsultaciju
 							</a>
 						</div>
 					</div>
@@ -50,7 +53,7 @@
 
 				<div class="relative lg:grid lg:grid-cols-3 lg:gap-x-8">
 					<div class="lg:col-span-1">
-						<h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">A better way to send money.</h2>
+						<h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Psihoterapija dostupna svima</h2>
 					</div>
 					<dl class="mt-10 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:gap-y-10 lg:mt-0 lg:col-span-2">
 						<div>
@@ -66,9 +69,9 @@
 										/>
 									</svg>
 								</div>
-								<p class="mt-5 text-lg leading-6 font-medium text-gray-900">Competitive rates</p>
+								<p class="mt-5 text-lg leading-6 font-medium text-gray-900">Izbor terapeuta</p>
 							</dt>
-							<dd class="mt-2 text-base text-gray-500">Consequuntur omnis dicta cumque, inventore atque ab dolores aspernatur tempora ab doloremque.</dd>
+							<dd class="mt-2 text-base text-gray-500">Nulti susret služi za procenu psihoterapijske motivacije, na osnovu čega naš tim efikasno odabere najboljeg terapeuta za vas.</dd>
 						</div>
 
 						<div>
@@ -84,9 +87,9 @@
 										/>
 									</svg>
 								</div>
-								<p class="mt-5 text-lg leading-6 font-medium text-gray-900">No hidden fees</p>
+								<p class="mt-5 text-lg leading-6 font-medium text-gray-900">Finansijska pristupačnost</p>
 							</dt>
-							<dd class="mt-2 text-base text-gray-500">Corporis quisquam nostrum nulla veniam recusandae temporibus aperiam officia incidunt at distinctio ratione.</dd>
+							<dd class="mt-2 text-base text-gray-500">Novac koji je neophodno izdvojiti za susret je daleko ispod redovne cene psihoterapije.</dd>
 						</div>
 
 						<div>
@@ -97,9 +100,9 @@
 										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
 									</svg>
 								</div>
-								<p class="mt-5 text-lg leading-6 font-medium text-gray-900">Instant transfers</p>
+								<p class="mt-5 text-lg leading-6 font-medium text-gray-900">Psihoterapijski pravci</p>
 							</dt>
-							<dd class="mt-2 text-base text-gray-500">Omnis, illo delectus? Libero, possimus nulla nemo tenetur adipisci repellat dolore eligendi velit doloribus mollitia.</dd>
+							<dd class="mt-2 text-base text-gray-500">Naši psihoterapeuti dolaze iz različitih psihoterapijskih pravaca.</dd>
 						</div>
 
 						<div>
@@ -110,9 +113,9 @@
 										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
 									</svg>
 								</div>
-								<p class="mt-5 text-lg leading-6 font-medium text-gray-900">Reminder emails</p>
+								<p class="mt-5 text-lg leading-6 font-medium text-gray-900">Dostupnost i fleksibilnost</p>
 							</dt>
-							<dd class="mt-2 text-base text-gray-500">Veniam necessitatibus reiciendis fugit explicabo dolorem nihil et omnis assumenda odit? Quisquam unde accusantium.</dd>
+							<dd class="mt-2 text-base text-gray-500">Naš tim okuplja veliki broj saradnika, pa je zakazivanje susreta moguće u terminima koji vama odgovaraju.</dd>
 						</div>
 					</dl>
 				</div>
@@ -145,13 +148,13 @@
 		<div class="bg-gray-50 my-14">
 			<div class="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
 				<div class="max-w-3xl mx-auto divide-y-2 divide-gray-200">
-					<h2 class="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">Frequently asked questions</h2>
+					<h2 class="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">Vi pitate, psihoterapeut odgovara</h2>
 					<dl class="mt-6 space-y-6 divide-y divide-gray-200">
 						<div x-data="{open: false}" class="pt-6">
 							<dt class="text-lg">
 								<!-- Expand/collapse question button -->
 								<button x-on:click="open = !open" type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
-									<span class="font-medium text-gray-900"> What&#039;s the best thing about Switzerland? </span>
+									<span class="font-medium text-gray-900"> Cena individualne psihoterapije</span>
 									<span class="ml-6 h-7 flex items-center">
 										<!--
                   Expand/collapse icon, toggle classes based on question open state.
@@ -167,7 +170,7 @@
 								</button>
 							</dt>
 							<dd x-show="open" class="mt-2 pr-12" id="faq-0">
-								<p class="text-base text-gray-500">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+								<p class="text-base text-gray-500">Za 60 minuta psihoterapije je neophodno izdvojiti minimum 1.000,00 dinara, kao donacije za rad savetovališta.</p>
 							</dd>
 						</div>
 
@@ -175,7 +178,7 @@
 							<dt class="text-lg">
 								<!-- Expand/collapse question button -->
 								<button x-on:click="open = !open" type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
-									<span class="font-medium text-gray-900"> What&#039;s the best thing about Switzerland? </span>
+									<span class="font-medium text-gray-900">Cena partnerske terapije </span>
 									<span class="ml-6 h-7 flex items-center">
 										<!--
                   Expand/collapse icon, toggle classes based on question open state.
@@ -191,14 +194,14 @@
 								</button>
 							</dt>
 							<dd x-show="open" class="mt-2 pr-12" id="faq-0">
-								<p class="text-base text-gray-500">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+								<p class="text-base text-gray-500">Za 90 minuta psihoterapije je neophodno izdvojiti po osobi minimum 1.000,00 dinara, kao donacije za rad savetovališta.</p>
 							</dd>
 						</div>
 						<div x-data="{open: false}" class="pt-6">
 							<dt class="text-lg">
 								<!-- Expand/collapse question button -->
 								<button x-on:click="open = !open" type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
-									<span class="font-medium text-gray-900"> What&#039;s the best thing about Switzerland? </span>
+									<span class="font-medium text-gray-900">Cena porodične psihoterapije</span>
 									<span class="ml-6 h-7 flex items-center">
 										<!--
                   Expand/collapse icon, toggle classes based on question open state.
@@ -214,14 +217,14 @@
 								</button>
 							</dt>
 							<dd x-show="open" class="mt-2 pr-12" id="faq-0">
-								<p class="text-base text-gray-500">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+								<p class="text-base text-gray-500">Cena porodične psihoterapije</p>
 							</dd>
 						</div>
 						<div x-data="{open: false}" class="pt-6">
 							<dt class="text-lg">
 								<!-- Expand/collapse question button -->
 								<button x-on:click="open = !open" type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
-									<span class="font-medium text-gray-900"> What&#039;s the best thing about Switzerland? </span>
+									<span class="font-medium text-gray-900">Cena porodične psihoterapije</span>
 									<span class="ml-6 h-7 flex items-center">
 										<!--
                   Expand/collapse icon, toggle classes based on question open state.
@@ -237,14 +240,16 @@
 								</button>
 							</dt>
 							<dd x-show="open" class="mt-2 pr-12" id="faq-0">
-								<p class="text-base text-gray-500">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+								<p class="text-base text-gray-500">
+									Prvi i neophodni uslov za započinjanje procesa psihoterapije je psihološka procena. To je intervju u trajanju od 90 minuta koji ima za cilj upoznavanje Vas kao klijenta i odabir najpodesnijeg stručnog saradnika za rad.
+								</p>
 							</dd>
 						</div>
 						<div x-data="{open: false}" class="pt-6">
 							<dt class="text-lg">
 								<!-- Expand/collapse question button -->
 								<button x-on:click="open = !open" type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
-									<span class="font-medium text-gray-900"> What&#039;s the best thing about Switzerland? </span>
+									<span class="font-medium text-gray-900">Cena psihološke procene</span>
 									<span class="ml-6 h-7 flex items-center">
 										<!--
                   Expand/collapse icon, toggle classes based on question open state.
@@ -260,14 +265,14 @@
 								</button>
 							</dt>
 							<dd x-show="open" class="mt-2 pr-12" id="faq-0">
-								<p class="text-base text-gray-500">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+								<p class="text-base text-gray-500">Za ovaj susret je neophodno izdvojiti 2.000,00 dinara donacije po osobi.</p>
 							</dd>
 						</div>
 						<div x-data="{open: false}" class="pt-6">
 							<dt class="text-lg">
 								<!-- Expand/collapse question button -->
 								<button x-on:click="open = !open" type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
-									<span class="font-medium text-gray-900"> What&#039;s the best thing about Switzerland? </span>
+									<span class="font-medium text-gray-900">Na koji način mogu izvršiti uplatu?</span>
 									<span class="ml-6 h-7 flex items-center">
 										<!--
                   Expand/collapse icon, toggle classes based on question open state.
@@ -283,7 +288,30 @@
 								</button>
 							</dt>
 							<dd x-show="open" class="mt-2 pr-12" id="faq-0">
-								<p class="text-base text-gray-500">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+								<p class="text-base text-gray-500"><a href="/donacije">Donacije</a> je moguće uplatiti uživo ili na žiro račun centra.</p>
+							</dd>
+						</div>
+						<div x-data="{open: false}" class="pt-6">
+							<dt class="text-lg">
+								<!-- Expand/collapse question button -->
+								<button x-on:click="open = !open" type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
+									<span class="font-medium text-gray-900">Radno vreme, da li mogu dolaziti vikendom?</span>
+									<span class="ml-6 h-7 flex items-center">
+										<!--
+                  Expand/collapse icon, toggle classes based on question open state.
+
+                  Heroicon name: outline/chevron-down
+
+                  Open: "-rotate-180", Closed: "rotate-0"
+                -->
+										<svg x-bind:class="{ 'rotate-180': open }" class="rotate-0 h-6 w-6 transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+											<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+										</svg>
+									</span>
+								</button>
+							</dt>
+							<dd x-show="open" class="mt-2 pr-12" id="faq-0">
+								<p class="text-base text-gray-500">Radno vreme centra je svakim danom, uključujući vikend, od 8-22h, a dostupnost termina zavisi od vašeg izabranog psihoterapeuta.</p>
 							</dd>
 						</div>
 
@@ -297,16 +325,14 @@
 			<div>
 				<div class="max-w-7xl mx-auto pb-10 px-4 sm:py-10 sm:px-6 lg:px-8">
 					<div class="text-center">
-						<h2 class="text-base font-semibold text-indigo-600 tracking-wide uppercase">Pricing</h2>
-						<p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">Take control of your team.</p>
-						<p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">Start building for free, then add a site plan to go live. Account plans unlock additional features.</p>
+						<h2 class="mt-1 text-3xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">Zakažite još danas svoj prvi razgovor sa psihoterapeutom</h2>
+						<p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">Vaše je samo da nas pozovete a mi ćemo se pobrinuti za ostalo</p>
 					</div>
 				</div>
 			</div>
 			<div class="max-w-7xl mx-auto px-4 pb-20" id="scroll">
 				<div class="relative bg-white shadow-xl">
-					<h2 class="sr-only">Contact us</h2>
-					>
+					<h2 class="sr-only">Kontakt informacije</h2>
 
 					<div class="grid grid-cols-1 lg:grid-cols-3">
 						<!-- Contact information -->
@@ -346,7 +372,7 @@
 							</div>
 							<div class="py-10 px-6 sm:px-10 xl:p-12">
 								<h3 class="text-lg font-medium text-white">Kontakt informacije</h3>
-								<p class="mt-6 text-base text-indigo-50 max-w-3xl">Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.</p>
+								<p class="mt-6 text-base text-indigo-50 max-w-3xl">Ubuntu Beograd brine o vašem mentalnom zdravlju od 2019. godine</p>
 								<dl class="mt-8 space-y-6">
 									<dt><span class="sr-only">Telefon</span></dt>
 									<dd class="flex text-base text-indigo-50">
@@ -367,12 +393,12 @@
 										<svg class="flex-shrink-0 w-6 h-6 text-indigo-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 											<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
 										</svg>
-										<span class="ml-3">support@workcation.com</span>
+										<span class="ml-3"><a href="psihoterapija-bg@ubuntucentar.com">psihoterapija-bg@ubuntucentar.com</a> </span>
 									</dd>
 								</dl>
 								<ul role="list" class="mt-8 flex space-x-12">
 									<li>
-										<a class="text-indigo-200 hover:text-indigo-100" href="https://sr-rs.facebook.com/UBUNTUcentar/">
+										<a class="text-indigo-200 hover:text-indigo-100" href="https://sr-rs.facebook.com/UBUNTUcentar/" target="_blank">
 											<span class="sr-only">Facebook</span>
 											<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" aria-hidden="true">
 												<path
@@ -383,7 +409,7 @@
 										</a>
 									</li>
 									<li>
-										<a class="text-indigo-200 hover:text-indigo-100" href="https://www.instagram.com/ubuntu_centar/?hl=en">
+										<a class="text-indigo-200 hover:text-indigo-100" href="https://www.instagram.com/ubuntu_centar/?hl=en" target="_blank">
 											<span class="sr-only">Instagram</span>
 											<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
 												<path
