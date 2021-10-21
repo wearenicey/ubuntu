6c22d10ad06d5c05c2c40543fd89b83d4c7cc591
Gongash92 <zarko.stevanovic092@gmail.com>
2021-10-21 10:00:35 +0200

    weew

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,12 +4,12 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
+// const tailwind = require('tailwindcss')
+// const purgecss = require('@fullhuman/postcss-purgecss')
 
-const postcssPlugins = [
-	tailwind(),
-]
+// const postcssPlugins = [
+// 	tailwind(),
+// ]
 
 // if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
 
