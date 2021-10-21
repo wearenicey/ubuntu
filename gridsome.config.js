5a0f98a91da88816b3e5ff749770078fc4b8e4ca
Gongash92 <zarko.stevanovic092@gmail.com>
2021-10-21 10:32:23 +0200

    test23

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,14 +4,14 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-// const tailwind = require('tailwindcss')
-// const purgecss = require('@fullhuman/postcss-purgecss')
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
 
-// const postcssPlugins = [
-// 	tailwind(),
-// ]
+const postcssPlugins = [
+	tailwind(),
+]
 
-// if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
 
 module.exports = {
 	siteName: 'Gridsome',
@@ -100,12 +100,12 @@ module.exports = {
 	chainWebpack: config => {
 		config.resolve.alias.set('@img', '@/assets/img')
 	},
-	// css: {
-	// 	loaderOptions: {
-	// 		postcss: {
-	// 			plugins: postcssPlugins,
-	// 		},
-	// 	},
-	// },
+	css: {
+		loaderOptions: {
+			postcss: {
+				plugins: postcssPlugins,
+			},
+		},
+	},
 }
 
