e02f855d2ca90a375d73fda97eb524af055cdf1b
Gongash92 <zarko.stevanovic092@gmail.com>
2021-10-21 10:55:21 +0200

    test

---
+++ dist/assets/js/page--src-pages-404-vue.e79d1509.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"7l0S":function(t,n,e){"use strict";e.r(n);var l=e("KHd+"),s=Object(l.a)({},(function(){var t=this.$createElement,n=this._self._c||t;return n("Layout",[n("h1",[this._v("404")])])}),[],!1,null,null,null);n.default=s.exports}}]);
