2280242f6876f1d1401c9d5e9f0e868013e5e3a8
Gongash92 <zarko.stevanovic092@gmail.com>
2021-10-21 16:01:40 +0200

    zasto ubuntu dio

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,54 +1,46 @@
 <template>
-	<div class="relative max-w-7xl mx-auto">
+	<div class="relative mx-auto max-w-7xl">
 		<div class="text-center">
-			<h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">Iz naše Blog radionice </h2>
+			<h2 class="text-3xl text-purple-800 sm:text-4xl">Iz naše Blog radionice</h2>
 			<p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">Aktuelni tekstovi na temu psihoterapije i psihologije, kao dodatak ličnom rastu i razvoju</p>
 		</div>
-		<div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-			<div v-for="post in $static.pocetna.edges" :key="post.node.id">
-				<div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-					<a :href="post.node.path">
+		<div v-for="post in $static.pocetna.edges" :key="post.node.id">
+			<div class="my-12 grid gap-16 pt-12 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12 items-center">
+				<div>
+					<div>
+						<a :href="post.node.category" class="inline-block">
+							<span class="inline-flex items-center rounded-full text-sm font-medium text-purple-800"> {{ post.node.category.title }} </span>
+						</a>
+					</div>
+					<a href="#" class="block mt-4">
+						<p class="text-xl font-semibold text-gray-900">{{ post.node.title }}</p>
+						<p class="mt-3 text-base text-gray-500">{{ post.node.excerpt }}</p>
+					</a>
+					<div class="mt-6 flex items-center">
 						<div class="flex-shrink-0">
-							<g-image class="h-48 w-full object-cover" :src="require(`!!assets-loader!@img/${post.node.image}`)" alt="" />
+							<a href="#">
+								<span class="sr-only">{{ post.node.author.name }}</span>
+								<img class="h-10 w-10 rounded-full" :src="post.node.author.image" alt="" />
+							</a>
 						</div>
-						<div class="flex-1 bg-white p-6 flex flex-col justify-between">
-							<div class="flex-1">
-								<p class="text-sm font-medium text-indigo-600">
-									<a :href="post.node.path" class="hover:underline"> </a>
-								</p>
-								<a :href="post.node.path" class="block mt-2">
-									<p class="text-xl font-semibold text-gray-900">
-										{{ post.node.title }}
-									</p>
-									<p class="mt-3 text-base text-gray-500">
-										{{ post.node.excerpt }}
-									</p>
-								</a>
-							</div>
-							<div class="mt-6 flex items-center">
-								<!-- <div class="flex-shrink-0">
-                  <a :href="post.author.href">
-                    <span class="sr-only">{{ post.author.name }}</span>
-                    <img
-                      class="h-10 w-10 rounded-full"
-                      :src="post.author.imageUrl"
-                      alt=""
-                    />
-                  </a>
-                </div> -->
-								<div class="">
-									<div class="flex space-x-1 text-sm text-gray-500">
-										<time :datetime="post.node.datetime">
-											{{ post.node.humanTime }}
-										</time>
-										<span aria-hidden="true"> &middot; </span>
-										<span> {{ post.node.timeToRead }} min read </span>
-									</div>
-								</div>
+						<div class="ml-3">
+							<p class="text-sm font-medium text-gray-900">
+								<a href="#"> {{ post.node.author.name }} </a>
+							</p>
+							<div class="flex space-x-1 text-sm text-gray-500">
+								<time :datetime="post.node.datetime">
+									{{ post.node.humanTime }}
+								</time>
+								<span aria-hidden="true"> &middot; </span>
+								<span> {{ post.node.timeToRead }} min </span>
 							</div>
 						</div>
+					</div>
+					<a :href="post.node.path" class="block mt-4">
+						<p class="mt-8 text-base text-green-600">Pročitaj više -></p>
 					</a>
 				</div>
+				<g-image class="h-96 w-full object-cover" :src="require(`!!assets-loader!@img/${post.node.image}`)" alt="" />
 			</div>
 		</div>
 	</div>
@@ -59,6 +51,11 @@
  
   edges {
     node {
+			category {
+					title
+					id
+			}
+
        title
        path
        excerpt
@@ -68,6 +65,10 @@
       featured
       humanTime: created(format: "DD.MM.YYYY")
       datetime: created
+			author {
+				name
+				image
+			}
     }
   }
 }
--- src/pages/ZastoUbuntu.vue
+++ src/pages/ZastoUbuntu.vue
@@ -1,6 +1,63 @@
 <template>
 	<Layout>
-		<div class="py-16 bg-gray-50 overflow-hidden">
+		<div class="py-16 overflow-hidden lg:py-24">
+			<div class="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+				<div class="relative mt-12 lg:mt-12 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+					<div class="relative">
+						<h1 class="text-3xl text-purple-800 tracking-tight sm:text-5xl">Šta Ubuntu čini drugačijim?</h1>
+						<p class="mt-6 text-lg text-gray-500">
+							Misija Ubuntu centra je da svima omogući lako dostupnu psihološku pomoć i lični rast i razvoj. Uz pomoć motivisanih i visokokvalifikovanih stručnjaka iz oblasti mentalnog zdravlja kroz savetovanje, psihoterapiju, radionice i
+							treninge zajedno podižemo kvalitet života naših klijenata.
+						</p>
+
+						<div class="mt-20 flex text-base max-w-prose mx-auto lg:max-w-none">
+							<div>
+								<a href="/kontakt/" class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-3xl text-white bg-green-600 hover:bg-indigo-700"> Kontaktirajte nas </a>
+							</div>
+						</div>
+					</div>
+
+					<div class="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+						<g-image class="relative mx-auto" width="490" src="~/assets/img/zasto-ubuntu-1.png" alt="" />
+					</div>
+				</div>
+
+				<div class="relative mt-12 sm:mt-16 lg:mt-32">
+					<div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+						<div class="lg:col-start-2">
+							<p class="mt-3 text-lg text-gray-500">
+								Kroz sistemsku podršku, redovnu superviziju, stručno usavršavanje i neformalna druženja vodimo brigu o našim saradnicima jer nam je iskustvo pokazalo da to rezultira kvalitetnim odnosima sa klijentima i uspešnom saradnjom.
+							</p>
+							<p class="mt-3 text-lg text-gray-500">Naša vizija je društvo u kome mentalno zdravlje, lični rast i razvoj i odlazak na psihoterapiju nisu tabu tema, već normalna svakodnevica.</p>
+						</div>
+
+						<div class="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
+							<g-image class="relative mx-auto" width="490" src="~/assets/img/zasto-ubuntu-2.png" alt="" />
+						</div>
+					</div>
+				</div>
+
+				<div class="relative mt-12 lg:mt-32 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+					<div class="relative">
+						<h2 class="text-3xl text-purple-800 tracking-tight sm:text-4xl">Mi smo tu da Vam pomognemo</h2>
+						<p class="mt-6 text-lg text-gray-500">Želeli bismo da psihoterapiju i naš ostali edukativni materijal učinimo dostupnim svim ljudima kojima su neophodni.</p>
+						<p class="mt-6 text-lg text-gray-500">Predano radimo na tome da naš Centar bude sinonim za zadovoljne i stručne saradnike i kvalitetan sadržaj dostupan svima.</p>
+
+						<div class="mt-20 flex text-base max-w-prose mx-auto lg:max-w-none">
+							<div>
+								<a href="/kontakt/" class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-3xl text-white bg-green-600 hover:bg-indigo-700"> Kontaktirajte nas </a>
+							</div>
+						</div>
+					</div>
+
+					<div class="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+						<g-image class="relative mx-auto" width="490" src="~/assets/img/zasto-ubuntu-3.png" alt="" />
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- <div class="py-16 bg-gray-50 overflow-hidden">
 			<div class="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
 				<div class="text-base max-w-prose mx-auto lg:max-w-none">
 					<h1 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Šta Ubuntu čini drugačijim?</h1>
@@ -23,13 +80,8 @@
 							<p>Želeli bismo da psihoterapiju i naš ostali edukativni materijal učinimo dostupnim svim ljudima kojima su neophodni.</p>
 							<p>Predano radimo na tome da naš Centar bude sinonim za zadovoljne i stručne saradnike i kvalitetan sadržaj dostupan svima.</p>
 						</div>
-						<div class="mt-10 flex text-base max-w-prose mx-auto lg:max-w-none">
-							<div class="rounded-md shadow">
-								<a href="/kontakt/" class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"> Kontaktirajte nas </a>
-							</div>
-						</div>
 					</div>
-					<div class="mt-12  relative text-base max-w-prose mx-auto lg:mt-0 lg:max-w-none">
+					<div class="mt-12 relative text-base max-w-prose mx-auto lg:mt-0 lg:max-w-none">
 						<svg
 							class="absolute top-0 right-0 -mt-20 -mr-20 lg:top-auto lg:right-auto lg:bottom-1/2 lg:left-1/2 lg:mt-0 lg:mr-0 xl:top-0 xl:right-0 xl:-mt-20 xl:-mr-20 md:visible invisible"
 							width="404"
@@ -70,26 +122,23 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		</div> -->
 		<div class="relative py-16 bg-white">
-			<div class="hidden absolute top-0 inset-x-0 h-1/2 bg-gray-50 lg:block" aria-hidden="true"></div>
+			<div class="hidden absolute top-0 inset-x-0 h-1/2 lg:block" aria-hidden="true"></div>
 			<div class="max-w-7xl mx-auto bg-indigo-600 lg:bg-transparent lg:px-8">
 				<div class="lg:grid lg:grid-cols-12">
 					<div class="relative z-10 lg:col-start-1 lg:row-start-1 lg:col-span-4 lg:py-16 lg:bg-transparent">
-						<div class="absolute inset-x-0 h-1/2 bg-gray-50 lg:hidden" aria-hidden="true"></div>
+						<div class="absolute inset-x-0 h-1/2  lg:hidden" aria-hidden="true"></div>
 						<div class="max-w-md mx-auto px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:p-0">
 							<div class="aspect-w-10 aspect-h-6 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1">
-								<g-image
-									class="object-cover object-center rounded-3xl shadow-2xl"
-									src="~/assets/img/zasto-ubuntu.jpg"
-									alt=""
-								/>
+								<g-image class="object-cover object-center rounded-3xl shadow-2xl" src="~/assets/img/zasto-ubuntu.jpg" alt="" />
 							</div>
 						</div>
 					</div>
 
-					<div class="relative bg-indigo-600 lg:col-start-3 lg:row-start-1 lg:col-span-10 lg:rounded-3xl lg:grid lg:grid-cols-10 lg:items-center">
+					<div class="relative bg-green-600 lg:col-start-3 lg:row-start-1 lg:col-span-10 lg:rounded-3xl lg:grid lg:grid-cols-10 lg:items-center">
 						<div class="hidden absolute inset-0 overflow-hidden rounded-3xl lg:block" aria-hidden="true">
+							
 							<svg class="absolute bottom-full left-full transform translate-y-1/3 -translate-x-2/3 xl:bottom-auto xl:top-0 xl:translate-y-0" width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
 								<defs>
 									<pattern id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
