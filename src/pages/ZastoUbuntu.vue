421c3fc0525ad4343eaba2b4dcc2835d04cf438b
Gongash92 <zarko.stevanovic092@gmail.com>
2021-10-22 12:03:54 +0200

    zasto ubuntu

--- src/pages/ZastoUbuntu.vue
+++ src/pages/ZastoUbuntu.vue
@@ -57,78 +57,65 @@
 			</div>
 		</div>
 
-		<!-- <div class="py-16 bg-gray-50 overflow-hidden">
-			<div class="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
-				<div class="text-base max-w-prose mx-auto lg:max-w-none">
-					<h1 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Šta Ubuntu čini drugačijim?</h1>
-				</div>
-				<div class="relative z-1 text-base max-w-prose mx-auto lg:max-w-5xl lg:mx-0 lg:pr-72">
-					<p class="text-lg text-gray-500">
-						Misija Ubuntu centra je da svima omogući lako dostupnu psihološku pomoć i lični rast i razvoj. Uz pomoć motivisanih i visokokvalifikovanih stručnjaka iz oblasti mentalnog zdravlja kroz savetovanje, psihoterapiju, radionice i
-						treninge zajedno podižemo kvalitet života naših klijenata.
-					</p>
-				</div>
-				<div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-start">
-					<div class="relative z-1">
-						<div class="prose prose-indigo text-gray-500 mx-auto lg:max-w-none">
-							<p>
-								Kroz sistemsku podršku, redovnu superviziju, stručno usavršavanje i neformalna druženja vodimo brigu o našim saradnicima jer nam je iskustvo pokazalo da to rezultira kvalitetnim odnosima sa klijentima i uspešnom saradnjom.
-							</p>
+		<section class="py-12 overflow-hidden md:py-20 lg:py-44">
+			<div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<svg class="hidden lg:block absolute top-full right-full transform translate-x-1/2 -translate-y-1/4" width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="13" cy="13" r="11.5" stroke="#F59E0B" stroke-width="3" />
+				</svg>
 
-							<p>Naša vizija je društvo u kome mentalno zdravlje, lični rast i razvoj i odlazak na psihoterapiju nisu tabu tema, već normalna svakodnevica.</p>
-							<h3>Mi smo tu da Vam pomognemo</h3>
-							<p>Želeli bismo da psihoterapiju i naš ostali edukativni materijal učinimo dostupnim svim ljudima kojima su neophodni.</p>
-							<p>Predano radimo na tome da naš Centar bude sinonim za zadovoljne i stručne saradnike i kvalitetan sadržaj dostupan svima.</p>
+				<svg class="hidden lg:block absolute transform translate-x-96 -top-24" width="33" height="34" viewBox="0 0 33 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M10.6804 2.9889C10.5364 2.54577 10.4645 2.3242 10.4801 2.1259C10.5009 1.8615 10.6259 1.6162 10.8275 1.44395C10.9788 1.31477 11.2004 1.24278 11.6435 1.09879C12.0866 0.95481 12.3082 0.882819 12.5065 0.898425C12.7709 0.919233 13.0162 1.04422 13.1884 1.24589C13.3176 1.39715 13.3896 1.61872 13.5336 2.06185L16.7783 12.0479L13.9251 12.975L10.6804 2.9889ZM11.357 18.0153L1.37086 21.26C0.92772 21.404 0.706152 21.4759 0.554897 21.6051C0.353225 21.7774 0.228237 22.0227 0.207428 22.2871C0.191822 22.4854 0.263813 22.7069 0.407798 23.1501C0.551782 23.5932 0.623773 23.8148 0.752956 23.966C0.925201 24.1677 1.1705 24.2927 1.4349 24.3135C1.6332 24.3291 1.85477 24.2571 2.29791 24.1131L12.284 20.8685L11.357 18.0153ZM17.0153 22.4856L20.26 32.4717C20.4039 32.9148 20.4759 33.1364 20.6051 33.2876C20.7774 33.4893 21.0227 33.6143 21.2871 33.6351C21.4854 33.6507 21.7069 33.5787 22.1501 33.4347C22.5932 33.2907 22.8148 33.2187 22.966 33.0896C23.1677 32.9173 23.2927 32.672 23.3135 32.4076C23.3291 32.2093 23.2571 31.9877 23.1131 31.5446L19.8684 21.5585L17.0153 22.4856ZM21.7946 17.7783L31.7807 14.5336C32.2238 14.3896 32.4454 14.3176 32.5966 14.1885C32.7983 14.0162 32.9233 13.7709 32.9441 13.5065C32.9597 13.3082 32.8877 13.0866 32.7437 12.6435C32.5997 12.2004 32.5277 11.9788 32.3986 11.8275C32.2263 11.6259 31.981 11.5009 31.7166 11.4801C31.5183 11.4645 31.2967 11.5365 30.8536 11.6804L20.8675 14.9251L21.7946 17.7783Z"
+						fill="#F59E0B"
+					/>
+				</svg>
+				<svg class="hidden lg:block absolute left-full transform -translate-x-1 -right-24" width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="32.5" cy="32.5" r="31" stroke="#F59E0B" stroke-width="3" />
+				</svg>
+				<svg class="hidden lg:block absolute -top-12 right-96" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="9.5" cy="9.5" r="8" stroke="#F59E0B" stroke-width="3" />
+				</svg>
+
+				<svg class="hidden lg:block absolute top-44 left-2/4" width="61" height="131" viewBox="0 0 61 131" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M8.25879 128.445C8.25879 128.445 33.3891 106.201 46.0405 71.9757C58.6919 37.7506 51.8965 1.72754 51.8965 1.72754" stroke="#F59E0B" stroke-width="2" stroke-linecap="square" />
+					<path d="M8.08008 129.125C8.08008 129.125 16.8082 125.157 23.5219 124.888C30.2355 124.62 34.0371 127.63 34.0371 127.63" stroke="#F59E0B" stroke-width="2" stroke-linecap="square" />
+					<path d="M8.49023 128.594C8.49023 128.594 10.9036 118.318 10.6236 113.455C10.3436 108.592 8.64905 102.541 8.64905 102.541" stroke="#F59E0B" stroke-width="2" stroke-linecap="square" />
+				</svg>
+
+				<div class="relative">
+					<blockquote class="mt-10">
+						<div class="max-w-3xl mx-auto text-center text-2xl leading-9 font-medium text-gray-900">
+							<p>&ldquo;Prihvatanje, podrška, uvažavanje i saosećanje su nam neophodni da bismo započeli proces zaceljenja i rasta. &rdquo;</p>
 						</div>
-					</div>
-					<div class="mt-12 relative text-base max-w-prose mx-auto lg:mt-0 lg:max-w-none">
-						<svg
-							class="absolute top-0 right-0 -mt-20 -mr-20 lg:top-auto lg:right-auto lg:bottom-1/2 lg:left-1/2 lg:mt-0 lg:mr-0 xl:top-0 xl:right-0 xl:-mt-20 xl:-mr-20 md:visible invisible"
-							width="404"
-							height="384"
-							fill="none"
-							viewBox="0 0 404 384"
-							aria-hidden="true"
-						>
-							<defs>
-								<pattern id="bedc54bc-7371-44a2-a2bc-dc68d819ae60" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-									<rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-								</pattern>
-							</defs>
-							<rect width="404" height="384" fill="url(#bedc54bc-7371-44a2-a2bc-dc68d819ae60)" />
-						</svg>
-						<blockquote class="relative bg-white rounded-lg shadow-lg">
-							<div class="rounded-t-lg px-6 py-8 sm:px-10 sm:pt-10 sm:pb-8">
-								<g-image src="~/assets/img/ubuntu-logo.png" alt="Workcation" class="h-14 w-14" />
-								<div class="relative text-lg text-gray-700 font-medium mt-8">
-									<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-gray-200" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
-										<path
-											d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"
-										/>
+						<footer class="mt-8">
+							<div class="md:flex md:items-center md:justify-center">
+								<div class="md:flex-shrink-0">
+									<g-image class="mx-auto h-10 w-10 rounded-full" src="~/assets/img/slika-1.jpg" alt="" />
+								</div>
+								<div class="mt-3 text-center md:mt-0 md:ml-4 md:flex md:items-center">
+									<div class="text-base font-medium text-gray-900">Miljana Vorkapić Viček</div>
+
+									<svg class="hidden md:block mx-1 h-5 w-5 text-indigo-600" fill="currentColor" viewBox="0 0 20 20">
+										<path d="M11 0h3L9 20H6l5-20z" />
 									</svg>
-									<p class="relative">Prihvatanje, podrška, uvažavanje i saosećanje su nam neophodni da bismo započeli proces zaceljenja i rasta.</p>
+
+									<div class="text-base font-medium text-gray-500">Supervizor, Ubuntu centar</div>
 								</div>
 							</div>
-							<cite class="relative flex items-center sm:items-start bg-indigo-600 rounded-b-lg not-italic py-5 px-6 sm:py-5 sm:pl-12 sm:pr-10 sm:mt-10">
-								<div class="relative rounded-full border-2 border-white sm:absolute sm:top-0 sm:transform sm:-translate-y-1/2">
-									<g-image class="w-12 h-12 sm:w-20 sm:h-20 rounded-full bg-indigo-300" src="~/assets/img/slika-1.jpg" alt="" />
-								</div>
-								<span class="relative ml-4 text-indigo-300 font-semibold leading-6 sm:ml-24 sm:pl-1">
-									<p class="text-white font-semibold sm:inline">Miljana Vorkapić Viček,</p>
-									<p class="sm:inline">Supervizor u Ubuntu Centru</p>
-								</span>
-							</cite>
-						</blockquote>
-					</div>
+						</footer>
+					</blockquote>
 				</div>
 			</div>
-		</div> -->
+		</section>
+
 		<div class="relative py-16 bg-white">
 			<div class="hidden absolute top-0 inset-x-0 h-1/2 lg:block" aria-hidden="true"></div>
-			<div class="max-w-7xl mx-auto bg-indigo-600 lg:bg-transparent lg:px-8">
+			<div class="max-w-7xl mx-auto bg-green-600 lg:bg-transparent lg:px-8">
 				<div class="lg:grid lg:grid-cols-12">
 					<div class="relative z-10 lg:col-start-1 lg:row-start-1 lg:col-span-4 lg:py-16 lg:bg-transparent">
-						<div class="absolute inset-x-0 h-1/2  lg:hidden" aria-hidden="true"></div>
+						<div class="absolute inset-x-0 h-1/2 bg-white lg:hidden" aria-hidden="true"></div>
 						<div class="max-w-md mx-auto px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:p-0">
 							<div class="aspect-w-10 aspect-h-6 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1">
 								<g-image class="object-cover object-center rounded-3xl shadow-2xl" src="~/assets/img/zasto-ubuntu.jpg" alt="" />
@@ -138,28 +125,40 @@
 
 					<div class="relative bg-green-600 lg:col-start-3 lg:row-start-1 lg:col-span-10 lg:rounded-3xl lg:grid lg:grid-cols-10 lg:items-center">
 						<div class="hidden absolute inset-0 overflow-hidden rounded-3xl lg:block" aria-hidden="true">
-							
-							<svg class="absolute bottom-full left-full transform translate-y-1/3 -translate-x-2/3 xl:bottom-auto xl:top-0 xl:translate-y-0" width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
-								<defs>
-									<pattern id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-										<rect x="0" y="0" width="4" height="4" class="text-indigo-500" fill="currentColor" />
-									</pattern>
-								</defs>
-								<rect width="404" height="384" fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+							<svg class="absolute bottom-full left-full transform translate-y-1/3 -translate-x-2/3 xl:bottom-auto xl:top-0 xl:translate-y-0" width="42" height="31" viewBox="0 0 42 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M10.9614 -33.171L59.642 7.91296L0.200058 30.1334L10.9614 -33.171Z" fill="#F1F58F" />
 							</svg>
-							<svg class="absolute top-full transform -translate-y-1/3 -translate-x-1/3 xl:-translate-y-1/2" width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
-								<defs>
-									<pattern id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-										<rect x="0" y="0" width="4" height="4" class="text-indigo-500" fill="currentColor" />
-									</pattern>
-								</defs>
-								<rect width="404" height="384" fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+
+							<svg class="absolute left-3/4 top-10" width="108" height="108" viewBox="0 0 108 108" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path
+									d="M35.9614 3.52831C46.6436 -0.250264 58.2477 -0.550111 69.1107 2.67175V2.67175C79.9737 5.89361 89.5379 12.4718 96.4325 21.4634V21.4634C103.327 30.4551 107.198 41.3986 107.491 52.7256V52.7256C107.784 64.0525 104.483 75.1814 98.0618 84.5171V84.5171C91.6408 93.8528 82.4291 100.916 71.7469 104.695V104.695C61.0648 108.473 49.4607 108.773 38.5977 105.551V105.551C27.7347 102.329 18.1705 95.7513 11.2759 86.7596V86.7596C4.38126 77.768 0.510216 66.8244 0.217533 55.4975V55.4975C-0.0751496 44.1705 3.22556 33.0417 9.64658 23.7059V23.7059C16.0676 14.3702 25.2793 7.30689 35.9614 3.52831V3.52831Z"
+									fill="#FE6600"
+								/>
+							</svg>
+
+							<svg class="absolute left-full transform -translate-x-20 translate-y-32" width="73" height="87" viewBox="0 0 73 87" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<rect width="47.8991" height="92.966" rx="23.9496" transform="matrix(0.829246 0.558884 -0.548292 0.836287 41.9727 -9.11035)" fill="#11A858" />
+							</svg>
+
+							<svg class="absolute top-3/4 left-full transform translate-y-1/3 -translate-x-full" width="75" height="103" viewBox="0 0 75 103" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<rect width="59.4361" height="115.358" rx="29.718" transform="matrix(0.970418 -0.241432 0.253756 0.967268 -6.2959 8.5144)" fill="#A377FF" />
+							</svg>
+
+							<svg class="absolute left-3/4 top-80" width="112" height="104" viewBox="0 0 112 104" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M0.919511 15.5038L111.126 0.432697L70.417 103.496L0.919511 15.5038Z" fill="#7DD3FC" />
+							</svg>
+
+							<svg class="absolute left-3/4 -bottom-1" width="83" height="41" viewBox="0 0 83 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path
+									d="M3.87677 25.3803C7.19301 17.4018 12.9499 10.678 20.3225 6.17247C27.695 1.66697 36.3047 -0.388888 44.9174 0.299551C53.5302 0.987993 61.704 4.38539 68.2672 10.0048C74.8304 15.6242 79.4462 23.1772 81.4529 31.5812C83.4597 39.9851 82.7544 48.8087 79.4381 56.7872C76.1219 64.7657 70.365 71.4895 62.9924 75.995C55.6199 80.5005 47.0103 82.5564 38.3975 81.868C29.7847 81.1795 21.611 77.7821 15.0477 72.1627C8.48451 66.5433 3.86874 58.9903 1.86199 50.5863C-0.144757 42.1824 0.560531 33.3588 3.87677 25.3803Z"
+									fill="#EC4899"
+								/>
 							</svg>
 						</div>
 						<div class="relative max-w-md mx-auto py-12 px-4 space-y-6 sm:max-w-3xl sm:py-16 sm:px-6 lg:max-w-none lg:p-0 lg:col-start-4 lg:col-span-6">
-							<h2 class="text-3xl font-extrabold text-white" id="join-heading">Pridruži se našem timu</h2>
+							<h2 class="text-3xl text-white" id="join-heading">Pridruži se našem timu</h2>
 							<p class="text-lg text-white">Ukoliko ste zainteresovani za rad sa nama, posetite našu stranice za karijeru i dodatno se informišite.</p>
-							<a class="block w-full py-3 px-5 text-center bg-white border border-transparent rounded-md shadow-md text-base font-medium text-indigo-700 hover:bg-gray-50 sm:inline-block sm:w-auto" href="/karijera/">Konkuriši za posao</a>
+							<a class="block w-full py-3 px-5 text-center bg-white border border-transparent rounded-md shadow-md text-base font-medium text-green-800 hover:bg-gray-50 sm:inline-block sm:w-auto" href="/karijera/">Konkuriši za posao</a>
 						</div>
 					</div>
 				</div>
