ccdd557214053a284648edb87a95e113452779e8
Goran <gbabarogic@hmail.com>
2021-10-27 20:09:55 +0200

    ispravke

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -103,7 +103,7 @@
 
 					<blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
 						<div class="relative text-lg font-medium text-gray-900 md:flex-grow">
-							<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-green-100" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+							<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-red-100" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
 								<path
 									d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"
 								/>
@@ -117,7 +117,7 @@
 								</div>
 								<div class="ml-4">
 									<div class="text-base font-medium text-gray-800">Marko</div>
-									<div class="text-base font-medium text-green-600">menadžer</div>
+									<div class="text-base font-medium text-gray-400">Menadžer</div>
 								</div>
 							</div>
 						</footer>
@@ -127,7 +127,7 @@
 
 					<blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
 						<div class="relative text-lg font-medium text-gray-900 md:flex-grow">
-							<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-green-100" fill="currentColor" viewBox="0 0 32 32">
+							<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-red-100" fill="currentColor" viewBox="0 0 32 32">
 								<path
 									d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"
 								/>
@@ -144,7 +144,7 @@
 								</div>
 								<div class="ml-4">
 									<div class="text-base font-medium text-gray-800">Mirjana</div>
-									<div class="text-base font-medium text-green-600">knjigovođa</div>
+									<div class="text-base font-medium text-gray-400">Knjigovođa</div>
 								</div>
 							</div>
 						</footer>
