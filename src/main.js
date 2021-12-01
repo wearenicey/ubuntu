bc2fe7d02cb720d572eb0f960898815b8b962ae2
Gongash92 <your_email_address@example.com>
2021-12-01 22:55:19 +0100

    global reload

--- src/main.js
+++ src/main.js
@@ -14,9 +14,6 @@ export default function (Vue, { router, head, isClient }) {
       reload: {
         inserted: function (el, binding, vnode) {
           el.addEventListener('click', (evt) => {
-            // the URL we want to go to will be in el.href for g-link, a, b-button.
-            // it will be in evt.target.href for b-nav-item and b-navbar-brand (because those are composite components
-            // that render as <li><a></li>, or something like that)
             if (process.isClient) {
               evt.preventDefault();
               window.location.href = evt.target.href || el.href;
