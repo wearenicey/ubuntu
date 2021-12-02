77e3ce1e4e51929f90f97ebcc28773c38f22a49c
babarogic <gbabarogic@gmail.com>
2021-12-02 09:42:12 +0100

    test #1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,10 @@
 	<div x-data="{ mobileMenuOpen: false }" class="relative bg-white">
 		<div class="flex max-w-7xl justify-between mx-auto items-center px-4 py-6 sm:px-6 md:space-x-10" id="position">
 			<div>
-				<a v-reload href="/" class="flex">
+				<g-link to="/" class="flex">
 					<span class="sr-only">Ubuntu Savetovaliste</span>
 					<img class="block h-12 w-auto sm:h-14" src="/logo.svg" alt="Ubuntu Savetovaliste" />
-				</a>
+				</g-link>
 			</div>
 			<div class="mr-2 my-2 md:hidden">
 				<button
@@ -64,12 +64,23 @@
 						>
 							<div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
 								<div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-									<a v-reload href="/savetovaliste-novi-sad/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-								
-										Novi Sad
-									</a>
+									<g-link to="/savetovaliste-novi-sad/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+										<!-- Heroicon name: outline/chart-bar -->
+										<!-- <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+											/>
+										</svg> -->
+										<div class="">
+											<p class="text-base font-medium text-gray-900">Novi Sad</p>
+											<!-- <p class="mt-1 text-sm text-gray-500">Novi Sad je vaš grad, treba vam psihoterapeut?</p> -->
+										</div>
+									</g-link>
 
-									<a v-reload href="/savetovaliste-beograd/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+									<g-link to="/savetovaliste-beograd/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 										<!-- Heroicon name: outline/cursor-click -->
 										<!-- <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 											<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
@@ -78,9 +89,9 @@
 											<p class="text-base font-medium text-gray-900">Beograd</p>
 											<!-- <p class="mt-1 text-sm text-gray-500">Beograd je vaš grad, a treba vam psihoterapeut? Ubuntu je pravi izbor</p> -->
 										</div>
-									</a>
+									</g-link>
 
-									<a v-reload href="/online-savetovaliste/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+									<g-link to="/online-savetovaliste/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 										<!-- Heroicon name: outline/shield-check -->
 										<!-- <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 											<path
@@ -94,7 +105,7 @@
 											<p class="text-base font-medium text-gray-900">Online</p>
 											<p class="mt-1 text-sm text-gray-500">Treba vam psiholog ili psihoterapeut online? Ubuntu je pravi izbor.</p>
 										</div>
-									</a>
+									</g-link>
 								</div>
 							</div>
 						</div>
@@ -141,7 +152,7 @@
 						>
 							<div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
 								<div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-									<a v-reload href="/zasto-ubuntu/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+									<g-link to="/zasto-ubuntu/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 										<!-- Heroicon name: outline/chart-bar -->
 										<!-- <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 											<path
@@ -155,9 +166,9 @@
 											<p class="text-base font-medium text-gray-900">Zašto Ubuntu?</p>
 											<p class="mt-1 text-sm text-gray-500">Misija Ubuntu centra je da svima omogući lako dostupnu psihološku pomoć</p>
 										</div>
-									</a>
+									</g-link>
 
-									<a v-reload href="/karijera/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+									<g-link to="/karijera/" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 										<!-- Heroicon name: outline/cursor-click -->
 										<!-- <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 											<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
@@ -166,13 +177,13 @@
 											<p class="text-base font-medium text-gray-900">Karijera</p>
 											<p class="mt-1 text-sm text-gray-500">Kako postati psihoterapeut? Savetovalište Ubuntu je pravi izbor</p>
 										</div>
-									</a>
+									</g-link>
 
-									<!-- <a href="/404" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+									<!-- <g-link to="/404" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 										<div class="">
 											<p class="text-base font-medium text-gray-900">Edukacije</p>
 										</div>
-									</a> -->
+									</g-link> -->
 								</div>
 							</div>
 						</div>
@@ -204,13 +215,13 @@
 							<div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
 								<div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
 									<div v-for="category in $static.category.edges" :key="category.node.id">
-										<a v-reload :href="category.node.path" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+										<g-link :to="category.node.path" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 										
 											<div class="">
 												<p class="text-base font-medium text-gray-900">{{ category.node.title }}</p>
 												<p class="mt-1 text-sm text-gray-500">{{ category.node.desc }}</p>
 											</div>
-										</a>
+										</g-link>
 									</div>
 								</div>
 								<div class="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
@@ -218,21 +229,21 @@
 										<p class="text-sm tracking-wide font-medium text-gray-500 uppercase">Poslednje objave</p>
 										<ul role="list" class="mt-4 space-y-4">
 											<li v-for="post in $static.blogs.edges" :key="post.node.id" class="text-base truncate">
-												<a v-reload :href="post.node.path" class="font-medium text-gray-900 hover:text-gray-700">
+												<g-link :to="post.node.path" class="font-medium text-gray-900 hover:text-gray-700">
 													{{ post.node.title }}
-												</a>
+												</g-link>
 											</li>
 										</ul>
 									</div>
 									<div class="mt-5 text-sm">
-										<a href="/blog" class="font-medium text-indigo-600 hover:text-indigo-500"> Pogledaj sve tekstove <span aria-hidden="true">&rarr;</span></a>
+										<g-link to="/blog" class="font-medium text-indigo-600 hover:text-indigo-500"> Pogledaj sve tekstove <span aria-hidden="true">&rarr;</span></g-link>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div> 
-					<a v-reload href="/donacije/" class="text-base font-medium text-gray-700 hover:text-gray-900">Donacije</a>
-					<a v-reload href="/kontakt/" class="text-base font-medium text-gray-700 hover:text-gray-900">Kontakt</a>
+					<g-link to="/donacije/" class="text-base font-medium text-gray-700 hover:text-gray-900">Donacije</g-link>
+					<g-link to="/kontakt/" class="text-base font-medium text-gray-700 hover:text-gray-900">Kontakt</g-link>
 				</nav>
 			</div>
 		</div>
@@ -280,42 +291,53 @@
 					</div>
 					<div class="mt-6">
 						<nav class="grid gap-y-8">
-							<a v-reload href="/savetovaliste-beograd/" class="-m-3 p-3 text-base font-medium text-gray-900 flex items-center rounded-md hover:bg-gray-50">
-							
-								Savetovalište Beograd
-							</a>
+							<g-link to="/savetovaliste-beograd/" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+								<!-- Heroicon name: outline/chart-bar -->
+								<!-- <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+									/>
+								</svg> -->
+								<span class="text-base font-medium text-gray-900"> Savetovalište Beograd </span>
+							</g-link>
 
-							<a v-reload href="/savetovaliste-novi-sad/" class="-m-3 text-base font-medium text-gray-900 p-3 flex items-center rounded-md hover:bg-gray-50">
-							
-								 Savetovalište Novi Sad
-							</a>
+							<g-link to="/savetovaliste-novi-sad/" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+								<!-- Heroicon name: outline/cursor-click -->
+								<!-- <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
+								</svg> -->
+								<span class="text-base font-medium text-gray-900"> Savetovalište Novi Sad </span>
+							</g-link>
 
-							<a v-reload href="/online-savetovaliste/" class="-m-3 p-3 text-base font-medium text-gray-900 flex items-center rounded-md hover:bg-gray-50">
-								 Online savetovalište 
-							</a>
+							<g-link to="/online-savetovaliste/" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+								<span class="text-base font-medium text-gray-900"> Online savetovalište </span>
+							</g-link>
 
-							<a v-reload href="/blog" class="-m-3 p-3 text-base font-medium text-gray-900 flex items-center rounded-md hover:bg-gray-50">
+							<g-link to="/blog" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
 						
-								Blog 
-							</a> 
+								<span class="text-base font-medium text-gray-900"> Blog </span>
+							</g-link> 
 						</nav>
 					</div>
 				</div>
 				<div class="py-6 px-5 space-y-6">
 					<div class="grid grid-cols-2 gap-y-4 gap-x-8">
-						<a href="/zasto-ubuntu/" class="text-base font-medium text-gray-900 hover:text-gray-700"> Zašto Ubuntu? </a>
+						<g-link to="/zasto-ubuntu/" class="text-base font-medium text-gray-900 hover:text-gray-700"> Zašto Ubuntu? </g-link>
 
-						<a href="/karijera/" class="text-base font-medium text-gray-900 hover:text-gray-700"> Karijera </a>
+						<g-link to="/karijera/" class="text-base font-medium text-gray-900 hover:text-gray-700"> Karijera </g-link>
 
-						<!-- <a href="/404" class="text-base font-medium text-gray-900 hover:text-gray-700"> Edukacije </a> -->
+						<!-- <g-link to="/404" class="text-base font-medium text-gray-900 hover:text-gray-700"> Edukacije </g-link> -->
 
-						<a href="/404" class="text-base font-medium text-gray-900 hover:text-gray-700"> Tim </a>
+						<g-link to="/404" class="text-base font-medium text-gray-900 hover:text-gray-700"> Tim </g-link>
 					</div>
 					<div>
-						<a href="/kontakt/" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-3xl shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"> Kontaktirajte nas </a>
+						<g-link to="/kontakt/" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-3xl shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"> Kontaktirajte nas </g-link>
 						<p class="mt-6 text-center text-base font-medium text-gray-500">
 							Želite da pomognete?
-							<a href="/donacije/" class="text-green-600 hover:text-green-500"> Donirajte </a>
+							<g-link to="/donacije/" class="text-green-600 hover:text-green-500"> Donirajte </g-link>
 						</p>
 					</div>
 				</div>
