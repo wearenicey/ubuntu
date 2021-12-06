884db29cf9ec93d99ac19b434da148c1e510369d
Gongash92 <zarko.stevanovic092@gmail.com>
2021-12-06 12:11:24 +0100

    test 4

---
+++ src/components/AllBlogs.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+		<div v-for="post in $static.sviPostovi.edges" :key="post.node.id" class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+			<a :href="post.node.path">
+				<div class="flex-shrink-0">
+					<g-image class="h-48 w-full object-cover" :src="require(`!!assets-loader!@img/${post.node.image}`)" alt="" />
+				</div>
+				<div class="flex-1 bg-white p-6 flex flex-col justify-between">
+					<div class="flex-1">
+						<p class="text-sm font-medium text-indigo-600">
+							<a v-reload :href="post.node.path" class="hover:underline"> </a>
+						</p>
+						<a v-reload :href="post.node.path" class="block mt-2">
+							<p class="text-xl font-semibold text-gray-900">
+								{{ post.node.title }}
+							</p>
+							<p class="mt-3 text-base text-gray-500">
+								{{ post.node.excerpt }}
+							</p>
+						</a>
+					</div>
+					<div class="mt-6 flex items-center">
+						<div class="">
+							<div class="flex space-x-1 text-sm text-gray-500">
+								<time :datetime="post.node.datetime">
+									{{ post.node.humanTime }}
+								</time>
+								<span aria-hidden="true"> &middot; </span>
+								<span> {{ post.node.timeToRead }} min čitanja </span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+	</div>
+</template>
+
+<static-query>
+query {
+  sviPostovi:  allPost  {
+ 
+  edges {
+    node {
+			category {
+					title
+					id
+					path
+			}
+
+       title
+       path
+       excerpt
+      image
+      pocetna  
+			timeToRead
+      featured
+      humanTime: created(format: "DD.MM.YYYY")
+      datetime: created
+			author {
+				name
+				image
+			}
+    }
+  }
+}
+}
+  
+
+</static-query>
