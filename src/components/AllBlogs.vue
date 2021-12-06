be0d69a60cf37780b408491394989029ee823742
Gongash92 <zarko.stevanovic092@gmail.com>
2021-12-06 12:30:52 +0100

    test

--- src/components/AllBlogs.vue
+++ src/components/AllBlogs.vue
@@ -1,37 +1,53 @@
 <template>
-	<div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-		<div v-for="post in $static.sviPostovi.edges" :key="post.node.id" class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-			<a :href="post.node.path">
-				<div class="flex-shrink-0">
-					<g-image class="h-48 w-full object-cover" :src="require(`!!assets-loader!@img/${post.node.image}`)" alt="" />
-				</div>
-				<div class="flex-1 bg-white p-6 flex flex-col justify-between">
-					<div class="flex-1">
-						<p class="text-sm font-medium text-indigo-600">
-							<a v-reload :href="post.node.path" class="hover:underline"> </a>
-						</p>
-						<a v-reload :href="post.node.path" class="block mt-2">
-							<p class="text-xl font-semibold text-gray-900">
-								{{ post.node.title }}
-							</p>
-							<p class="mt-3 text-base text-gray-500">
-								{{ post.node.excerpt }}
-							</p>
-						</a>
+		<div class="relative max-w-7xl mx-auto px-6 mt-20 sm:px-0">
+		<div class="text-center">
+			<h1 class="text-3xl text-gray-800 sm:text-4xl">Ubuntu Blog</h1>
+			<p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">Naši terapeuti pišu za vas</p>
+		</div>
+		<div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+			<div v-for="post in $static.sviPostovi.edges" :key="post.node.id" class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+				<a :href="post.node.path">
+					<div class="flex-shrink-0">
+						<g-image class="h-48 w-full object-cover" :src="require(`!!assets-loader!@img/${post.node.image}`)" alt="" />
 					</div>
-					<div class="mt-6 flex items-center">
-						<div class="">
-							<div class="flex space-x-1 text-sm text-gray-500">
-								<time :datetime="post.node.datetime">
-									{{ post.node.humanTime }}
-								</time>
-								<span aria-hidden="true"> &middot; </span>
-								<span> {{ post.node.timeToRead }} min čitanja </span>
+					<div class="flex-1 bg-white p-6 flex flex-col justify-between">
+						<div class="flex-1">
+							<p class="text-sm font-medium text-indigo-600">
+								<a :href="post.node.path" class="hover:underline"> </a>
+							</p>
+							<a :href="post.node.path" class="block mt-2">
+								<p class="text-xl font-semibold text-gray-900">
+									{{ post.node.title }}
+								</p>
+								<p class="mt-3 text-base text-gray-500">
+									{{ post.node.excerpt }}
+								</p>
+							</a>
+						</div>
+						<div class="mt-6 flex items-center">
+							<!-- <div class="flex-shrink-0">
+                  <a :href="post.author.href">
+                    <span class="sr-only">{{ post.author.name }}</span>
+                    <img
+                      class="h-10 w-10 rounded-full"
+                      :src="post.author.imageUrl"
+                      alt=""
+                    />
+                  </a>
+                </div> -->
+							<div class="">
+								<div class="flex space-x-1 text-sm text-gray-500">
+									<time :datetime="post.node.datetime">
+										{{ post.node.humanTime }}
+									</time>
+									<span aria-hidden="true"> &middot; </span>
+									<span> {{ post.node.timeToRead }} min read </span>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</a>
+				</a>
+			</div>
 		</div>
 	</div>
 </template>
@@ -52,9 +68,7 @@ query {
        path
        excerpt
       image
-      pocetna  
 			timeToRead
-      featured
       humanTime: created(format: "DD.MM.YYYY")
       datetime: created
 			author {
--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,20 +1,6 @@
 <template>
 	<Layout>
-		<div class="relative pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-			<div class="absolute inset-0">
-				<div class="bg-white h-1/3 sm:h-2/3" />
-			</div>
-			<div class="relative max-w-7xl mx-auto">
-				<div class="text-center">
-					<h1 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl uppercase">Ubuntu Blog</h1>
-					<div>
-						<div>
-							<p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">Naši terapeuti pišu za vas</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<AllBlogs/>
 	</Layout>
 </template>
 
