42b96143c0e1f58840d45677a9ac80cd71185788
Gongash92 <zarko.stevanovic092@gmail.com>
2021-12-08 10:21:16 +0100

    test 3

---
+++ src/components/ScrollB.vue
@@ -0,0 +1,41 @@
+<template>
+<client-only> 
+	<vue-horizontal>
+		<section class="py-6" v-for="cate in $static.allCategory.edges" :key="cate.node.id">
+			<g-link v-reload :to="cate.node.path" class="px-5 rounded-md py-3 bg-gray-100 mr-3">{{ cate.node.title }}</g-link>
+		</section>
+	</vue-horizontal>
+</client-only>
+</template>
+
+<static-query>
+query {
+  allCategory {
+    edges {
+      node {
+        path
+        id
+        title
+      }
+    }
+  }
+}
+</static-query>
+
+<script>
+// Depending on how you installed it, import it if required.
+// import VueHorizontal from "vue-horizontal";
+
+export default {
+	// components: { VueHorizontal },
+	data() {
+		return { items: [1, 2, 3] };
+	},
+};
+</script>
+<style scoped>
+a.active--exact.active {
+	color: #059669;
+	font-weight: bold;
+}
+</style>
