6371e28c59462bf3a33cdde45980d5d449f1d45e
Gongash92 <zarko.stevanovic092@gmail.com>
2022-01-21 15:39:05 +0100

    ispravka

--- src/pages/SavetovalisteNoviSad.vue
+++ src/pages/SavetovalisteNoviSad.vue
@@ -127,8 +127,8 @@
 
 			<div class="bg-purple-800 ">
 				<div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-24 lg:px-8 lg:flex lg:items-center lg:justify-between">
-					<h2 class="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl">
-						<span class="block text-white">Spremni za podršku? </span>
+					<h2 class="text-3xl font-sans tracking-tight text-gray-900 md:text-4xl">
+						<span class="block text-white font-bold">Spremni za podršku? </span>
 						<span class="block text-white">Upoznajte novosadske psihoterapeute</span>
 					</h2>
 					<div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
