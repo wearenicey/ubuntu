5212c06abdce1bbc03c93a2156dd3a961acc9370
dejan.krcmar <dejan.krcmar@gmail.com>
2024-12-27 19:07:41 +0100

    izmena kontakt podataka i koraci u psihoteraopija ns

--- src/pages/SavetovalisteNoviSad.vue
+++ src/pages/SavetovalisteNoviSad.vue
@@ -330,11 +330,196 @@
 
 		<div class="bg-primary" id="cenovnik">
 			<div class="px-4 pt-12 sm:px-6 lg:px-8 lg:pt-20">
+
+				
+			
 				<div class="text-center">
 					<h2 class="text-3xl font-sans leading-6 text-gray-300">Cenovnik</h2>
 					<!-- <p class="mt-2 text-3xl font-serif tracking-tight text-white sm:text-4xl lg:text-5xl">Odaberi paket koji ti odgovara</p> -->
 				</div>
 			</div>
+
+			<div class="mt-16 bg-white pb-12 lg:mt-20 lg:pb-20">
+				<div class="relative z-0">
+					<div class="absolute inset-0 h-5/6 bg-primary lg:h-2/3" />
+					<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+
+						
+						<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+						<!-- <div class="py-6">
+  <div class="sm:hidden">
+    <label for="tabs" class="sr-only">Select a tab</label>
+   
+    <select id="tabs" name="tabs" class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500">
+      <option selected>Novi Sad</option>
+      <option>Beograd</option>
+      <option>Online</option>
+    </select>
+  </div>
+  <div class="hidden sm:block">
+    <nav class="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
+  
+      <a href="#" class="text-gray-900 rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10" aria-current="page">
+        <span>Novi Sad</span>
+        <span aria-hidden="true" class="bg-indigo-500 absolute inset-x-0 bottom-0 h-0.5"></span>
+      </a>			
+      <a href="#" class="text-gray-500 hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10">
+        <span>Beograd</span>
+        <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10">
+        <span>Online</span>
+        <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
+      </a>
+    </nav>
+  </div>
+</div> -->
+
+<section class="-mt-32 max-w-7xl mx-auto relative z-10 pb-32 px-4 sm:px-6 lg:px-8"
+				aria-labelledby="contact-heading">
+				<h2 class="sr-only" id="contact-heading">Kontaktirajte nas</h2>
+				<div class="grid grid-cols-1 gap-y-8 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-8">
+					<div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+						<div class="flex-shrink-0">
+							<g-image class="h-48 w-full object-cover" src="~/assets/img/novisad.jpg"
+								alt="psihološko savetovalište novi sad" />
+						</div>
+						<div class="flex-1 bg-white p-6 flex flex-col justify-between">
+							<div class="flex-1">
+								
+									<h3 class="text-xl font-sans font-medium text-gray-900">1. korak: Kontaktirajte nas</h3>
+									 <p class="mt-3 text-base text-gray-500">Ako želite da procena za terapiju i susreti sa terapeutom budu uživo u Novom Sadu pozovite nas na broj <a href="tel:+381658286881">065 828 6881</a>
+									 </p>
+									 <p class="mt-3 text-base text-gray-500">Za online psihološku procenu i online susrete sa terapeutom možete pisati na našu email adresu <a href="mailto:psihoterapija-onlajn@ubuntucentar.com">psihoterapija-onlajn@ubuntucentar.com</a>
+									 </p>
+								
+							</div>
+						</div>
+						<div class="flex-1 bg-gray-50 p-6 flex">
+							<p class="text-base font-medium text-gray-900 italic">Nakon zakazanog termina, na Whatsapp ili Viber u pdf formatu dobijate detaljne informacije o radu
+							</p>
+						</div>
+					</div>
+
+					<div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+						<div class="flex-shrink-0">
+							<g-image class="h-48 w-full object-cover" src="~/assets/img/beograd.jpg"
+								alt="psihološko savetovalište beograd" />
+						</div>
+						<div class="flex-1 bg-white p-6 flex flex-col justify-between">
+							<div class="flex-1">
+								
+									<h3 class="text-xl font-sans font-medium text-gray-900">2. korak: Procena za psihoterapiju</h3>
+									<p class="mt-3 text-base text-gray-500">Procena se sastoji iz procene ličnosti i motivacije za terapijski rad.
+										Razgovor se obavlja u trajanju od 90min.</p>
+									<p class="mt-3 text-base text-gray-500">Uživo na adresi Anđe Ranković 6.</p>	
+									<p class="mt-3 text-base text-gray-500">Za online procenu neophodno je obezbediti adekvatne uslove za rad:
+									<span class="font-bold">stabilnu internet konekciju i izolovanu prostoriju koja će obezbediti neometanu komunikaciju i privatnost.</span></p>	
+							</div>
+						</div>
+						<div class="flex-1 bg-gray-50 p-6 flex">
+							<p class="text-base font-medium text-gray-900 italic">Cilj procene jeste odabir terapeuta koji će najbolje odgovoriti na vaše potrebe. Podaci dobijeni na osnovu procene imaju isključivo internu upotrebu.</p>
+						</div>
+					</div>
+
+					<div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+						<div class="flex-shrink-0">
+							<g-image class="h-48 w-full object-cover" src="~/assets/img/onilne.jpg" alt="online psihoterapija" />
+						</div>
+						<div class="flex-1 bg-white p-6 flex flex-col justify-between">
+							<div class="flex-1">
+							
+									<h3 class="text-xl font-sans font-medium text-gray-900">3. korak: Prva seansa</h3>
+									<p class="mt-3 text-base text-gray-500">Kada odaberemo terapeuta za vas, dobićete njegov kontakt telefon kako biste
+										zakazali prvi susret.</p>
+						
+							</div>
+						</div>
+						<div class="flex-1 bg-gray-50 p-6 flex">
+							<p class="text-base font-medium text-gray-900 italic">Na kontakt telefon terapeuta, u zavisnosti od projekta, čeka se najduže 2 nedelje.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 py-8">
+    <h2 class="font-headline text-2xl font-semibold sm:text-3xl text-center">Revamp Your Website in Three Steps</h2>
+
+    <div class="grid sm:grid-cols-3 gap-16 sm:gap-8 mt-16 sm:mt-24">
+        <div class="text-left">
+            <div class="relative w-20 h-20 sm:w-28 sm:h-28 lg:w-32 lg:h-32 mx-auto left-2">
+                <div
+                    class="z-10 relative w-full h-full bg-white rounded-full border border-gray-300 shadow flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5"
+                        stroke="currentColor" class="text-gray-500 w-1/2 h-1/2" alt="Import icon">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z">
+                        </path>
+                    </svg>
+                </div>
+                <div class="absolute inset-0 -translate-x-2 -translate-y-2 bg-blue-500 rounded-full"></div>
+            </div>
+            <h3 class="font-headline text-xl sm:text-2xl mt-6 sm:mt-10 text-center">Kontaktirajte nas</h3>
+            <p class="leading-relaxed mt-4">Ako želite da procena za terapiju i susreti sa terapeutom budu uživo u Novom Sadu pozovite nas na broj 065 828 6881</p>
+						<p class="leading-relaxed mt-4">Za online psihološku procenu i online susrete sa terapeutom možete pisati na našu email adresu psihoterapija-onlajn@ubuntucentar.com</p>
+						<p class="leading-relaxed mt-4 italic">Nakon zakazanog termina, na Whatsapp ili Viber u pdf formatu dobijate detaljne informacije o radu</p>
+        </div>
+        <div class="text-left">
+            <div class="relative w-20 h-20 sm:w-28 sm:h-28 lg:w-32 lg:h-32 mx-auto left-2">
+                <div
+                    class="z-10 relative w-full h-full bg-white rounded-full border border-gray-300 shadow flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5"
+                        stroke="currentColor" class="text-gray-500 w-1/2 h-1/2" alt="Translate icon">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"></path>
+                    </svg>
+                </div>
+                <div class="absolute inset-0 -translate-x-2 -translate-y-2 bg-blue-500 rounded-full"></div>
+            </div>
+            <h3 class="font-headline text-xl sm:text-2xl mt-6 sm:mt-10 text-center">Procena za psihoterapiju</h3>
+            <p class="leading-relaxed mt-4">Procena se sastoji iz procene ličnosti i motivacije za terapijski rad. Razgovor se obavlja u trajanju od 90min.</p>
+						<p class="leading-relaxed mt-4">Uživo na adresi Anđe Ranković 6.</p>
+						<p class="leading-relaxed mt-4">Za online procenu neophodno je obezbediti adekvatne uslove za rad: stabilnu internet konekciju i izolovanu prostoriju koja će obezbediti neometanu komunikaciju i privatnost.</p>
+						<p class="leading-relaxed mt-4">Cilj procene jeste odabir terapeuta koji će najbolje odgovoriti na vaše potrebe. Podaci dobijeni na osnovu procene imaju isključivo internu upotrebu</p>
+        </div>
+        <div class="text-left">
+            <div class="relative w-20 h-20 sm:w-28 sm:h-28 lg:w-32 lg:h-32 mx-auto left-2">
+                <div
+                    class="z-10 relative w-full h-full bg-white rounded-full border border-gray-300 shadow flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width=".5"
+                        stroke="currentColor" class="text-gray-500 w-1/2 h-1/2" alt="Export icon">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 9.75v6.75m0 0l-3-3m3 3l3-3m-8.25 6a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z">
+                        </path>
+                    </svg>
+                </div>
+                <div class="absolute inset-0 -translate-x-2 -translate-y-2 bg-blue-500 rounded-full"></div>
+            </div>
+            <h3 class="font-headline text-xl sm:text-2xl mt-6 sm:mt-10 text-center">Prva seansa</h3>
+            <p class="leading-relaxed mt-4">Kada odaberemo terapeuta za vas, dobićete njegov kontakt telefon kako biste
+							zakazali prvi susret.</p>
+						<p class="leading-relaxed mt-4">Na kontakt telefon terapeuta, u zavisnosti od projekta, čeka
+							se najduže 2 nedelje.</p>
+        </div>
+    </div>
+</div>
 
 			<div class="mt-16 bg-white pb-12 lg:mt-20 lg:pb-20">
 				<div class="relative z-0">
@@ -781,7 +966,7 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		
 
 		<div class="bg-white py-16 lg:py-24">
 			<div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
